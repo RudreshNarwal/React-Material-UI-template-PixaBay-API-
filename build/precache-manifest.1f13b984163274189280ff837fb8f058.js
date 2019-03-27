5167cc33228c6bd68d58e9a4c3f6a05609daa21f
RudreshNarwal <rudresh.narwal@gmail.com>
2019-03-27 15:47:04 +0530

    updates

---
+++ build/precache-manifest.1f13b984163274189280ff837fb8f058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c8b71f74efefcf04502",
+    "url": "/static/js/main.ea65360f.chunk.js"
+  },
+  {
+    "revision": "280146e47807cf6a9748",
+    "url": "/static/js/2.48bee78c.chunk.js"
+  },
+  {
+    "revision": "8c8b71f74efefcf04502",
+    "url": "/static/css/main.996ff41e.chunk.css"
+  },
+  {
+    "revision": "f1a01cbe9739242c45ae0ba1a622fa1b",
+    "url": "/index.html"
+  }
+];
